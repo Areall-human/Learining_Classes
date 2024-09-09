dfa1328f52f63f4ed8f95531152c72a2f2837905
Abdulydoool <gbolahanbusari25@gmail.com>
2024-09-09 14:32:10 +0100

    Initial commit

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,83 @@ namespace Classes
         //this is a constructor, it is similar to a method, it's nameed the same as the class, it's placed, it has no retun, and it's always executed once an objecct of class is createed
         //it's format is "private" then "var type" then and _ before (no spaces) "var name" then "=" then "var value" and finallly ";"
         //"private hides the variable from other classes like the program class"
-        private string _model = "";
+        //below are backing fields for their respective properties ie _model is a backing field for modem
+        //backing fields can be omitte when ther's no value to be gotten or set
+        //the value of _model is not alterd at any point(not MANUALLY gotten or set), so it can function without a backing field
+        /*private string _model = "";*/
+        //the above can be subtitued with public string Model {  get; set; }
         private string _brand = "";
-        
+        //the same applies for _is luxury
+        /*private bool _isLuxury;*/
+
         // this is a constructor, it's called when an object of that class is created
         // in the parentheses of a public constructor is ment for passing data that will serve as the paremiter
-        public Car(string brand,string model) 
+
+        //this two sets of code are, sorrounding properties, they allow _model to be indirectly accessed via the public string, "Model" and also prevent direct access to "_model"
+
+
+        //this is a lambda expression
+        //public bool IsLuxury { get => _isLuxury; set => _isLuxury = value; }
+        public bool IsLuxury{ get; set; }
+        //it does the same thing as 
+
+        /*
+        public bool isLuxury
         {
-            _brand = brand;
-            _model = model;
+            get
+            {
+                return _isLuxury;
+            }
+            set
+            {
+                _isLuxury = value;
+            }
+        }
+        */
+        /*public string Model { get => _model; set => _model = value; }*/
+        public string Model {  get; set; }
+
+        public string Brand {/*getting brand*/ 
+            get
+            {
+                if (IsLuxury)
+                {
+                    return _brand + " -Luxury Eddition";
+                }
+                else
+                {
+                    return _brand;
+                }
+            }
+            
+            set 
+            
+            {
+
+                //put a break point at the if statement to see the "value" in action
+                //brand setiing 
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You've entered nothing!");
+                    _brand = "Brand is DEFAULTVALUE";
+                }else 
+                {
+                    _brand = value; 
+                }
+
+            }
+                
+                
+                }
+
+        
+
+        //this a custom constructor
+        public Car(string brand,string model, bool isluxury) 
+        {
+            Brand = brand;
+            Model = model;
+            IsLuxury = isluxury; 
             Console.WriteLine($"A {brand} car of model {model} was created ");
         }
     }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,10 +4,22 @@
     {
         static void Main(string[] args)
         {
+            
+
             //creates an instance/object of car, it executes all the coee in the summoned class
-            Car audi = new Car("Audi", "A3");
-            Car bmw = new Car("BMW", "M4");
-            Console.WriteLine("Hello, World!");
+            Car audi = new Car("Audi", "A3", true);
+
+
+            Car bmw = new Car("BMW", "M4", false);
+
+
+            Console.WriteLine("please enter the brand name");
+            //Brand setting 
+            audi.Brand = Console.ReadLine();
+            //Brand getting
+            Console.WriteLine("you entered " + audi.Brand);
+
+            
         }
     }
 }
